3ec583b7b6d3f7a02c1093752ed4e88d6c97460e
DarkReap7 <67169193+DarkReap7@users.noreply.github.com>
2021-03-09 17:25:44 +0300

    uploaded task

---
+++ Anagrams/src/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+        Reverser reverser = new Reverser();
+
+        String reversedString = reverser.letterAnagramProducer("abcd e1gh");
+        System.out.println(reversedString);
+    }
+}
